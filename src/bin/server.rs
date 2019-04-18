a39ad841c75ae74b3762746c3ec18a764e504926
Ellie Frost <web@stillinbeta.com>
2019-04-18 19:04:30 -0400

    We can now handle multiple connections

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -3,7 +3,8 @@ extern crate termiontelnet;
 extern crate tokio;
 
 use futures::Stream;
-use tokio::net::{TcpListener, TcpStream};
+use termiontelnet::{ServerEvents, TelnetOption};
+use tokio::net::TcpListener;
 use tokio::prelude::*;
 
 #[derive(Debug)]
@@ -18,22 +19,29 @@ fn main() {
 
     let server = listener
         .incoming()
+        .map_err(|e| eprintln!("An error occured: {:?}", e))
         .for_each(|sock| {
+            let ip_addr: u16 = sock.peer_addr().map(|p| p.port()).unwrap_or(0);
             let framed = termiontelnet::TelnetCodec::framed(sock);
 
             let (sink, stream) = framed.split();
 
-            sink.send(termiontelnet::ServerEvents::IACDoNAWS)
-                .and_then(|_sink| {
-                    stream
-                        .for_each(|msg| {
-                            println!("Received {:?}", msg);
-                            future::ok(())
-                        })
-                        .map(|_| eprintln!("connection closed"))
+            let task = sink
+                .send_all(futures::stream::iter_ok::<_, std::io::Error>(vec![
+                    ServerEvents::Do(TelnetOption::WindowSize),
+                    ServerEvents::Will(TelnetOption::Echo),
+                    ServerEvents::Do(TelnetOption::LineMode),
+                ]))
+                .and_then(move |_sink| {
+                    stream.for_each(move |msg| {
+                        println!("Received {:?} from {}", msg, ip_addr);
+                        future::ok(())
+                    })
                 })
-        })
-        .map_err(|e| eprintln!("An error occured: {:?}", e));
+                .map_err(|e| eprintln!("An error occured: {:?}", e));
+
+            tokio::spawn(task)
+        });
 
     tokio::run(server);
 }
